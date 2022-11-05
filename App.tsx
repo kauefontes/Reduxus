b6dbb1fce7af4c25e40013ce2760c4d9d9a73500
Kaue Pereira <kauefontes@outlook.com>
2022-11-04 22:08:14 -0400

    feat: visualizing created componen

--- App.tsx
+++ App.tsx
@@ -1,11 +1,17 @@
-import { StatusBar } from 'expo-status-bar'
-import { Text, View } from 'react-native'
+import React from 'react'
+import { Box, NativeBaseProvider } from 'native-base'
+import { Provider as ReduxProvider } from 'react-redux'
+import { UserList } from './src/screens/UserList'
+import store from './src/store'
 
 export default function App() {
   return (
-    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NativeBaseProvider>
+      <ReduxProvider store={store}>
+        <Box flex={1} alignItems='center' justifyContent='center'>
+          <UserList />
+        </Box>
+      </ReduxProvider>
+    </NativeBaseProvider>
   )
 }
